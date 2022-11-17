94b7eda9982f9cd653590ff6d8f22ea210901d7b
KMS9612 <kimdev9612@gmail.com>
2022-11-17 19:23:09 +0900

    #39-Feat/파이어베이스 연동-회원정보 가져오기와서 세션에 저장하기  완료

--- pages/myPage/edit/index.tsx
+++ pages/myPage/edit/index.tsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
-import MyPageADContainer from "../../../src/component/units/MyPage/Advertiser/MPAD.container";
-import MyPageIFContainer from "../../../src/component/units/MyPage/Influencer/MPIF.container";
+import { useEffect, useState } from "react";
+import MyPageADContainer from "../../../src/component/units/MyPage/Edit/Advertiser/MPAD.container";
+import MyPageIFContainer from "../../../src/component/units/MyPage/Edit/Influencer/MPIF.container";
 
 export default function MyPage() {
-  const [isAD, setIsAD] = useState(false);
-  return isAD ? <MyPageADContainer /> : <MyPageIFContainer />;
+  const [isAD, setIsAD] = useState("");
+  useEffect(() => {
+    const ref = sessionStorage.getItem("userValue");
+    if (ref === "인플루언서" || ref === "광고주") {
+      setIsAD(ref);
+    }
+  }, []);
+
+  return isAD !== "인플루언서" ? <MyPageADContainer /> : <MyPageIFContainer />;
 }
