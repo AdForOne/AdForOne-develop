74e40148176602af1fe54d50c6796b9bcc9ba65a
KMS9612 <kimdev9612@gmail.com>
2022-11-24 12:23:48 +0900

    #62-Feat/파이어베이스 연동 개인페이지 완성(가격정보 받아오기, 태그 readOnly설정 해야한다)

--- src/component/units/MyPage/Read/Influencer/MPIFRead.container.tsx
+++ src/component/units/MyPage/Read/Influencer/MPIFRead.container.tsx
@@ -2,30 +2,32 @@ import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { db } from "../../../../../common/firebase/firebase.config";
-import usePage from "../../../../../common/firebase/firebase.myPage";
+import { useIsSsr } from "../../../../../common/hooks/useSrr";
 import MPIFReadPresenter from "./MPIFRead.presenter";
 
 export default function MPIFReadContainer() {
-  const [Link, setLink] = useState();
-  const [ServiceMain, setServiceMain] = useState();
-  const [userInfo, setUserInfo] = useState({});
+  const isSsr = useIsSsr();
+  const router = useRouter();
 
   // db에 mypage정보 가져오는 함수
   const fetchPage = async () => {
-    const router = useRouter();
     const PageRef = doc(db, "myPage", `${router.query?.email}`);
     const PageSnap = await getDoc(PageRef);
     const UserInfo = doc(db, "users", `${router.query?.email}`);
     const UserSnap = await getDoc(UserInfo);
-
-    setUserInfo({
-      uid: UserSnap.data()?.uid,
-    });
-    setLink(PageSnap.data()?.Link);
-    setServiceMain(PageSnap.data()?.ServiceMain);
-    console.log(userInfo);
+    localStorage.setItem("Link", PageSnap.data()?.Link);
+    localStorage.setItem("ServiceMain", PageSnap.data()?.ServiceMain);
+    localStorage.setItem("email", UserSnap.data()?.email);
+    localStorage.setItem("CheckCategory", UserSnap.data()?.CheckCategory);
+    localStorage.setItem("SNSLink", UserSnap.data()?.SNSLink);
+    localStorage.setItem("UsedSNS", UserSnap.data()?.UsedSNS);
+    localStorage.setItem("value", UserSnap.data()?.value);
   };
+  useEffect(() => {
+    fetchPage();
+  }, []);
 
-  fetchPage();
-  return <MPIFReadPresenter Link={Link} ServiceMain={ServiceMain} />;
+  if (localStorage.getItem("ServiceMain")) {
+    return <MPIFReadPresenter />;
+  }
 }
