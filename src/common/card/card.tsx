c11c3c1383c159030328de179b18107322e0abbb
KMS9612 <kimdev9612@gmail.com>
2022-11-25 21:44:27 +0900

    #65-Feat/파이어베이스 연동-마이페이지메인,리스트연결(db기획 수정 필요)

--- src/common/card/card.tsx
+++ src/common/card/card.tsx
@@ -6,7 +6,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-export default function CardContainer() {
+export default function CardContainer(props: any) {
   return (
     <div>
       <Card sx={{ maxWidth: 345 }}>
@@ -20,12 +20,9 @@ export default function CardContainer() {
           <Typography gutterBottom variant="h5" component="div">
             인플루언서
           </Typography>
-          <Typography sx={{ mb: 1.5 }} color="#1976D2">
-            2022-10-25 12:15
-          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="#1976D2"></Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {props.Link}
           </Typography>
         </CardContent>
         <CardActions>
