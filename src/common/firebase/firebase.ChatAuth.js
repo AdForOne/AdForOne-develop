67dd9b5fd533320ecdb6455d9ff4114d5a5deb25
Jae-hong-Lee <keash8091@gmail.com>
2022-12-16 19:38:24 +0900

    ChatBtn 수정

--- src/common/firebase/firebase.ChatAuth.js
+++ src/common/firebase/firebase.ChatAuth.js
@@ -4,10 +4,8 @@ export default function useLoginAuth() {
   const router = useRouter();
   const LoginAuth = (uid) => {
     if (uid) {
-      console.log(uid);
       return router.push(`/chatting/${sessionStorage.uid}`);
     } else {
-      console.log("error");
       router.push("/login");
       alert("로그인해주세요");
     }
