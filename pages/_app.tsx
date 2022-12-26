e6ccb23d9edfd29a43a337267c901b2ca50d3d4e
KMS9612 <kimdev9612@gmail.com>
2022-12-26 20:53:24 +0900

    129-Feat/이미지 버그픽스,콘솔 제거, 회원가입 이미지 버튼 늘어나는 버그픽스

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import "../styles/globals.css";
 import React, { useState } from "react";
-import { RecoilRoot, useRecoilState } from "recoil";
+import { RecoilRoot } from "recoil";
 import Layout from "../src/common/layout";
 import { AppProps } from "next/app";
-import { AccessToken } from "../src/common/recoil/token";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/component/commons/ChatBtnVal/index.tsx
+++ src/component/commons/ChatBtnVal/index.tsx
@@ -1,10 +1,8 @@
 export const ValidationURL = (session: string, router: string) => {
   const EditRouter = "edit";
-  // console.log("채팅버튼", EditRouter, session, router);
   if (EditRouter === router || session === router) {
     return "채팅목록";
   } else {
-    console.log("JS 채팅버튼", EditRouter, session, router);
     return "채팅걸기";
   }
 };
